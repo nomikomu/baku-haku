620f3f04dc6db5da9e0a57daabf7ffd41e424575
defm03 <yutsuro@gmail.com>
2013-02-25 19:10:04 +0100

    Generalizing by adding objects class

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ SCREEN_WIDTH = 88
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 25
 
+class Object:
+	def __init__(self, x, y, char, color):
+		self.x = x
+		self.y = y
+		self.char = char
+		self.color = color
+		
+	def move(self, dx, dy):
+		#move by the given amount
+		self.x += dx
+		self.y += dy
+		
+	def draw(self):
+		#set the color and then draw the character that represents this object at its position
+		libtcod.console_set_default_foreground(con, self.color)
+		libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+		
+	def clear(self):
+		#erase the character that represents this object
+		libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE) 
+		
+
 def handle_keys():
-	global playerx, playery
-	
-	#movement keys
-	
-	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-		playery -= 1
-		
-	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-		playery += 1
-		
-	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-		playerx -= 1
-	
-	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-		playerx += 1
-		
-	#fullscrn mode; game q
+	#key = libtcod.console_check_for_keypress()  #real-time
+	#key = libtcod.console_wait_for_keypress(True)  #turn-based
 	
 	key = libtcod.console_check_for_keypress()
 	if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -31,6 +38,24 @@ def handle_keys():
 		
 	elif key.vk == libtcod.KEY_ESCAPE:
 		return True #exit game
+	
+	#movement keys
+	
+	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+		player.move(0, -1)
+		
+	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+		player.move(0, 1)
+		
+	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+		player.move(-1, 0)
+	
+	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+		player.move(1, 0)
+		
+	#fullscrn mode; game q
+	
+	
 
 #							 #
 # initialization & main loop #
@@ -40,18 +65,27 @@ libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | 
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'kubrck-rl', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
 #player pos
 playerx = SCREEN_WIDTH/2
 playery = SCREEN_HEIGHT/2
 
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.dark_amber)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.violet)
+
+objects = [npc, player]
+
 while not libtcod.console_is_window_closed():
-	libtcod.console_set_default_foreground(0, libtcod.dark_amber)
-	libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+	#draw all obj in the list
+	for object in objects:
+		object.draw()
 	
+	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 	libtcod.console_flush()
 	
-	libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
-	#clears out after '@' mark //temp
+	for object in objects:
+		object.clear()
 	
 	#handle keys and exit game if needed
 	exit = handle_keys()
